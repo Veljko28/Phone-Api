9dd5c6303fad31ea3a294ada749e23ca1fa8554e
Veljko28 <charlibear28@gmail.com>
2021-06-23 12:19:04 +0200

    Deleting template code and adding installers

---
+++ Phone-Api/Installers/InstallExtentions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phone_Api.Installers
+{
+	public static class InstallExtentions
+	{
+		public static void installServicesInAssembly(this IServiceCollection services, IConfiguration Configuration)
+		{
+			var installers = typeof(Startup).Assembly.ExportedTypes.Where(x => typeof(IInstaller).IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract)
+					  .Select(Activator.CreateInstance).Cast<IInstaller>().ToList();
+
+			foreach (var installer in installers)
+			{
+				installer.InstallServices(Configuration, services);
+			}
+		}
+	}
+}
