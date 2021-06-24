6800cfe7079d0a2b7abf383463a71b749a0b30ab
Veljko28 <charlibear28@gmail.com>
2021-06-24 13:37:49 +0200

    Adding Db Context for EF core

---
+++ Phone-Api.Repository/Services/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Phone_Api.Models.Responses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phone_Api.Repository.Services
+{
+	public class AppDbContext : DbContext
+	{
+		private readonly IConfiguration configuration;
+
+		public AppDbContext(IConfiguration configuration) : base()
+		{
+			this.configuration = configuration;
+		}
+
+		public DbSet<PhoneResponse> Phones { get; set; }
+
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+		}
+	}
+}
