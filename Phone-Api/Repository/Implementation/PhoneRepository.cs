1d66ada25fa41a3cd7f87e9200eb78b62a6d5825
Veljko28 <charlibear28@gmail.com>
2021-06-25 13:17:37 +0200

    Adding the repository folder

---
+++ Phone-Api/Repository/Implementation/PhoneRepository.cs
@@ -0,0 +1,50 @@
+﻿using Phone_Api.Models.Requests;
+using Phone_Api.Models.Responses;
+using Phone_Api.Repository.Interfaces;
+using Phone_Api.Repository.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phone_Api.Repository.Implementation
+{
+	public class PhoneRepository : IPhoneRepository
+	{
+		private readonly AppDbContext _context;
+
+		public PhoneRepository(AppDbContext context)
+		{
+			_context = context;
+		}
+		public async Task<bool> AddPhoneAsync(PhoneRequest phone)
+		{
+			PhoneResponse phoneResponse = new PhoneResponse
+			{
+				Id = Guid.NewGuid().ToString(),
+				Name = phone.Name,
+				Description = phone.Description,
+				Price = phone.Price,
+				DateCreated = phone.DateCreated,
+				Exires = phone.Exires
+			};
+
+			var response = await _context.Phones.AddAsync(phoneResponse);
+			await _context.SaveChangesAsync();
+
+			return response != null;
+		}
+
+		public async Task<PhoneResponse> GetPhoneInfoByIdAsync(string Id)
+		{
+			var response = await _context.Phones.FindAsync(Id);
+			return response;
+		}
+
+		public async Task<IEnumerable<PhoneResponse>> SearchPhonesAsync(string search)
+		{
+			var response = await _context.Phones.FindAsync(search);
+			return new List<PhoneResponse> { response };
+		}
+	}
+}
---
+++ Phone-Api/Repository/Services/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Phone_Api.Models.Responses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phone_Api.Repository.Services
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		{
+
+		}
+		public DbSet<PhoneResponse> Phones { get; set; }
+	}
+}
