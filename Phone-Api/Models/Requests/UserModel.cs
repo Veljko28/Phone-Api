4e1fd246d74d12bac0a3319fec54675ce416f16e
Veljko28 <charlibear28@gmail.com>
2021-06-26 12:24:12 +0200

    Login and User Models for register and login

---
+++ Phone-Api/Models/Requests/UserModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phone_Api.Models.Responses
+{
+	public class UserModel
+	{
+		[Required]
+		[MinLength(15), MaxLength(256)]
+		[EmailAddress]
+		public string Email { get; set; }
+		[Required]
+		[MinLength(6), MaxLength(256)]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+		[Required]
+		[MinLength(3), MaxLength(50)]
+		public string UserName { get; set; }
+	}
+}
