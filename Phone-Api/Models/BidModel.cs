51fcc35f5ac1cd901bcd3b15d6fcf99c6f65cb94
Veljko28 <charlibear28@gmail.com>
2021-07-16 12:03:53 +0200

    Request for the bid and the bid model itself

---
+++ Phone-Api/Models/BidModel.cs
@@ -0,0 +1,20 @@
+﻿using Phone_Api.Models.Responses;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phone_Api.Models
+{
+	public class BidModel
+	{
+		[Required]
+		public string Id { get; set; }
+		[Required]
+		public string UserName { get; set; }
+		[Required]
+		public Decimal BidAmount { get; set; }
+
+	}
+}
