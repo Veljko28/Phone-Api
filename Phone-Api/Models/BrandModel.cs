cbd48b06b5a2051800ed7bc3b43d2a7ba57e6bfe
Veljko28 <charlibear28@gmail.com>
2021-07-16 12:05:07 +0200

    Brand and Category Enums for phone req

---
+++ Phone-Api/Models/BrandModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phone_Api.Models
+{
+	public enum BrandModel
+	{
+		Google,
+		Apple,
+		Samsung,
+		Vivo,
+		Htc,
+		Alcatel
+	}
+}
---
+++ Phone-Api/Models/CategoryModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phone_Api.Models
+{
+	public enum CategoryModel
+	{
+		Android_Phone,
+		IOS_Phone,
+		Others
+	}
+}
