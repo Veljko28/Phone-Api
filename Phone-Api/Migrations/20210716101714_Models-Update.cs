ffc5dcb9abb58c2a7fd8906219755125e24cb173
Veljko28 <charlibear28@gmail.com>
2021-07-16 12:21:54 +0200

    Updating the database with the new models

---
+++ Phone-Api/Migrations/20210716101714_Models-Update.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Phone_Api.Migrations
+{
+    public partial class ModelsUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
