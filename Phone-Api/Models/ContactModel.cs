e403c0d9b5504be31ce2d97b1ae73b7a71988425
Veljko28 <charlibear28@gmail.com>
2021-07-16 12:06:03 +0200

    Contact and Rating models for UserModel
    email and phone contact and star ratings

---
+++ Phone-Api/Models/ContactModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phone_Api.Models
+{
+	public class ContactModel
+	{
+		[Required]
+		public string PhoneNumber { get; set; }
+		[Required]
+		public string Email { get; set; }
+
+	}
+}
---
+++ Phone-Api/Models/RatingModel.cs
@@ -0,0 +1,26 @@
+﻿using Phone_Api.Models.Responses;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phone_Api.Models
+{
+	public class RatingModel
+	{
+		[Required]
+		[MinLength(1),MaxLength(5)]
+		public int Stars { get; set; }
+
+		[Required]
+		public UserModel Reviewer { get; set; }
+
+		[Required]
+		public string PhoneName { get; set; }
+		
+		[Required]
+		public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+
+	}
+}
