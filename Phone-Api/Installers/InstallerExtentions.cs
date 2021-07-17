5d10099aa623c6390fa0dda6d07e7cc4846956ea
Veljko28 <charlibear28@gmail.com>
2021-07-17 11:08:19 +0200

    Installers Redo
    Creating the istaller again

---
+++ Phone-Api/Installers/InstallerExtentions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phone_Api.Installers
+{
+	public static class InstallerExtentions
+	{
+		public static void InstallServicesInAssembly(this IServiceCollection services, IConfiguration Configuration)
+		{
+			var installers = typeof(Startup).Assembly.ExportedTypes.Where(x => typeof(IInstaller).IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract)
+					  .Select(Activator.CreateInstance).Cast<IInstaller>().ToList();
+
+			foreach (var installer in installers)
+			{
+				installer.InstallServices(Configuration, services);
+			}
+		}
+
+	}
+}
