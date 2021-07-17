ff2f39b81e89c54760677c775d48f22cee373b03
Veljko28 <charlibear28@gmail.com>
2021-07-17 13:39:37 +0200

    Phones table

---
+++ PhoneDb/Tables/Phones.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Phones]
+(
+	[Id] NVARCHAR(50) NOT NULL PRIMARY KEY, 
+    [Image] NVARCHAR(50) NULL, 
+    [Name] NVARCHAR(100) NOT NULL,
+    [DateCreated] DATETIME2 NOT NULL DEFAULT getutcdate(), 
+    [Seller] NVARCHAR(50) NOT NULL, 
+    [Category] NVARCHAR(50) NOT NULL, 
+    [Brand] NVARCHAR(25) NOT NULL,
+
+)
