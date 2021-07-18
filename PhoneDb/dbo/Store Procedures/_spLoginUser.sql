a9bcd57e66e2d30601f9141a2cae3b559a69190a
Veljko28 <charlibear28@gmail.com>
2021-07-18 12:53:15 +0200

    User store procedures
    These store procedures are used in the user repository for login in and registering

---
+++ PhoneDb/dbo/Store Procedures/_spLoginUser.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[_spLoginUser]
+	@Email_UserName NVARCHAR(150),
+	@Password NVARCHAR(150)
+AS
+begin
+	SELECT * FROM [dbo].[Users] WHERE (Email = @Email_UserName OR UserName = @Email_UserName) AND [Password] = @Password;
+end
---
+++ PhoneDb/dbo/Store Procedures/_spRegisterUser.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[_spRegisterUser]
+	@Email NVARCHAR(150),
+	@UserName NVARCHAR(150),
+	@Password NVARCHAR(150)
+AS
+begin
+	INSERT INTO [dbo].[Users] (Email, UserName, [Password]) VALUES (@Email, @UserName, @Password);
+end
