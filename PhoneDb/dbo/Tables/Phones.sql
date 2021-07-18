be25a95ee20ab3058c86101e6f95fadd119f950b
Veljko28 <charlibear28@gmail.com>
2021-07-18 12:51:52 +0200

    Upgraded tables
    Tables with new fields and better optimization

--- PhoneDb/dbo/Tables/Phones.sql
+++ PhoneDb/dbo/Tables/Phones.sql
@@ -1,8 +1,9 @@
 ﻿CREATE TABLE [dbo].[Phones]
 (
 	[Id] NVARCHAR(50) NOT NULL PRIMARY KEY, 
-    [Image] NVARCHAR(50) NULL, 
+    [Image] NVARCHAR(150) NULL, 
     [Name] NVARCHAR(100) NOT NULL,
+	[Description] NVARCHAR(256) NULL, 
     [DateCreated] DATETIME2 NOT NULL DEFAULT getutcdate(), 
     [Seller] NVARCHAR(50) NOT NULL, 
     [Category] NVARCHAR(50) NOT NULL, 
---
+++ PhoneDb/dbo/Tables/Users.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Table1]
+(
+	[Id] NVARCHAR(50) NOT NULL PRIMARY KEY,
+	[Image] NVARCHAR(256) NOT NULL,
+	[Email] NVARCHAR(150) NOT NULL,
+	[UserName] NVARCHAR(150) NOT NULL,
+	[Password] NVARCHAR(150) NOT NULL, 
+	[Description] NVARCHAR(256) NULL, 
+	[Phones_sold] int NOT NULL DEFAULT 0,
+	[Contact] NVARCHAR(50) NULL,
+)
