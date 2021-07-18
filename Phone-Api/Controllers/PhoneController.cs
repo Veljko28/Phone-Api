be1528240421a8747feac81f66bcfd696df04d3e
Veljko28 <charlibear28@gmail.com>
2021-07-18 12:51:04 +0200

    Default Controllers
    Implemented Phone and User controllers

---
+++ Phone-Api/Controllers/PhoneController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Phone_Api.Helpers;
+using Phone_Api.Models.Requests;
+using Phone_Api.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phone_Api.Controllers
+{
+	public class PhoneController : Controller
+	{
+		private readonly IPhoneRepository _phones;
+
+		public PhoneController(IPhoneRepository phones)
+		{
+			_phones = phones;
+		}
+
+		[HttpPost(ApiRoutes.PhoneRoutes.Add)]
+		public async Task<IActionResult> AddPhone([FromBody] PhoneRequest phoneRequest, [FromBody] string userId)
+		{
+			var phone = await _phones.addPhoneAsync(phoneRequest, userId);
+
+			if (phone == null)
+			{
+				return BadRequest("Error while creating the phone");
+			}
+
+			return Ok(phone);
+		}
+
+		[HttpGet(ApiRoutes.PhoneRoutes.GetById)]
+		public async Task<IActionResult> GetById([FromRoute] string phoneId)
+		{
+			return Ok("phone id = " + phoneId);
+
+			var phone = await _phones.getPhoneByIdAsync(phoneId);
+
+			if (phone == null)
+			{
+				return BadRequest("No phone was found with id " + phoneId);
+			}
+
+			return Ok(phone);
+		}
+	}
+}
---
+++ Phone-Api/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Phone_Api.Helpers;
+using Phone_Api.Models;
+using Phone_Api.Models.Requests;
+using Phone_Api.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phone_Api.Controllers
+{
+	public class UserController : Controller
+	{
+		private readonly IUserRepository _users;
+
+		public UserController(IUserRepository users)
+		{
+			_users = users;
+		}
+
+		[HttpPost(ApiRoutes.UserRoutes.Register)]
+		public async Task<IActionResult> Register([FromBody] UserRequest userRequest)
+		{
+			bool registered = await _users.RegisterAsync(userRequest);
+
+			if (!registered)
+			{
+				return BadRequest("Failed to register");
+			}
+
+			return Ok("Successfully registered !");
+		}
+
+		[HttpPost(ApiRoutes.UserRoutes.Login)]
+		public async Task<IActionResult> Login([FromBody] LoginRequest loginRequest)
+		{
+			UserModel user = await _users.LoginAsync(loginRequest);
+
+			if (user == null)
+			{
+				return BadRequest("Failed to Login");
+			}
+
+			return Ok(user);
+		}
+	}
+}
