010cc8617c4b572f1a5b16da996849230d058679
Veljko28 <charlibear28@gmail.com>
2021-07-18 12:52:27 +0200

    Store Procedures for phone actions
    These store procedures are used in the phone repository

---
+++ PhoneDb/dbo/Store Procedures/_spAddPhone.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[_spAddPhone]
+	@Id NVARCHAR(50),
+	@Seller NVARCHAR(50),
+	@Name NVARCHAR(100),
+	@Image NVARCHAR(150),
+	@Category NVARCHAR(50),
+	@Brand NVARCHAR(25)
+AS
+begin
+	INSERT INTO [dbo].[Phones] (Id, Seller, [Name], [Image], Category, Brand) VALUES (@Id, @Seller, @Name, @Image, @Category, @Brand);
+end
---
+++ PhoneDb/dbo/Store Procedures/_spFindPhoneByID.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[_spFindPhoneByID]
+	@Id NVARCHAR(50)
+AS
+begin
+	SELECT * FROM [dbo].[Phones] WHERE Id = @Id;
+end	
