d5b76675ae5953a1a84a4af3f5e6eacb879e9853
Veljko28 <charlibear28@gmail.com>
2021-07-18 12:47:32 +0200

    User and Phone Model
    New Models Project with start models

---
+++ Phone-Api.Models/PhoneModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phone_Api.Models
+{
+	public class PhoneModel
+	{
+		public string Id { get; set; }
+		public string Image { get; set; }
+		public string Name { get; set; }
+		public string Seller { get; set; }
+		public string Category { get; set; }
+		public string Brand { get; set; }
+	}
+}
---
+++ Phone-Api.Models/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phone_Api.Models
+{
+	public class UserModel
+	{
+		public string Id { get; set; }
+		public string Image { get; set; }
+		public string Email { get; set; }
+		public string UserName { get; set; }
+		public string Password { get; set; }
+		public string Description { get; set; }
+		public int Phones_sold { get; set; }
+		public string Contanct{ get; set; }
+	}
+}
