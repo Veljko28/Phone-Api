e14d3bf13365c07212e1cdfbea454131d3498b8e
Veljko28 <charlibear28@gmail.com>
2021-07-18 12:48:56 +0200

    Interface repositories
    Phone and User interface repos for dependency injection

---
+++ Phone-Api.Repository/Interfaces/IPhoneRepository.cs
@@ -0,0 +1,16 @@
+﻿using Phone_Api.Models;
+using Phone_Api.Models.Requests;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phone_Api.Repository.Interfaces
+{
+	public interface IPhoneRepository
+	{
+
+		Task<PhoneModel> addPhoneAsync(PhoneRequest phoneRequest, string userId);
+		Task<PhoneModel> getPhoneByIdAsync(string Id);
+	}
+}
---
+++ Phone-Api.Repository/Interfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Phone_Api.Models;
+using Phone_Api.Models.Requests;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phone_Api.Repository.Interfaces
+{
+	public interface IUserRepository
+	{
+		Task<bool> RegisterAsync(UserRequest userRequest);
+
+		Task<UserModel> LoginAsync(LoginRequest loginRequest);
+	}
+}
