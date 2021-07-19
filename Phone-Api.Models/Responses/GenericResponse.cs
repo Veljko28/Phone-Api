c6d147f0ceed574011347861efa7090d4bc8176a
Veljko28 <charlibear28@gmail.com>
2021-07-19 19:45:09 +0200

    GenericResponse Method
    Contains ErrorMessage and Success

---
+++ Phone-Api.Models/Responses/GenericResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phone_Api.Models.Responses
+{
+	public class GenericResponse
+	{
+		public bool Success { get; set; }
+
+		public string ErrorMesage { get; set; }
+	}
+}
