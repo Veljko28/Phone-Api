ba21d7b28b8bf2e7cb683cb848ffc9000c7a0f0e
Veljko28 <charlibear28@gmail.com>
2021-07-19 19:46:34 +0200

    ChangePassword Code
    Repository implementation and request class

--- Phone-Api.Repository/UserRepository.cs
+++ Phone-Api.Repository/UserRepository.cs
@@ -2,11 +2,13 @@
 using Microsoft.Extensions.Configuration;
 using Phone_Api.Models;
 using Phone_Api.Models.Requests;
+using Phone_Api.Models.Responses;
 using Phone_Api.Repository.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,32 +22,141 @@ namespace Phone_Api.Repository
 			_configuration = configuration;
 		}
 
-		public async Task<UserModel> LoginAsync(LoginRequest loginRequest)
+		public async Task<GenericResponse> ChangePasswordAsync(string userId, ChangePasswordRequest change)
 		{
-			string sql = "exec _spLoginUser @Email_UserName @Password";
+			if (change.Old_Password != change.Confirm_Old_Password)
+				return new GenericResponse
+				{
+					Success = false,
+					ErrorMesage = "The Passwords do not match"
+				};
+
+			string findUserPassword = "exec [_spGetUserById] @Id";
 
 			using (SqlConnection db = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
 			{
 				await db.OpenAsync();
 
-				UserModel user = (await db.QueryAsync<UserModel>(sql, loginRequest)).FirstOrDefault();
+				UserModel user = (await db.QueryAsync<UserModel>(findUserPassword, new { Id = userId })).FirstOrDefault();
 
-				return user;
+				db.Close();
+
+				if (user == null) return new GenericResponse { Success = false, ErrorMesage = "Cannot find user !"};
+
+				string dbPassword = user.Password;
+
+				if (!PasswordHashing.ComparePasswords(dbPassword, change.Old_Password))
+						return new GenericResponse { Success = false, ErrorMesage = "Incorrect Password !" };
+
+				string passwordHash = PasswordHashing.HashPassword(change.New_Password);
+
+				await db.OpenAsync();
+
+				int rowsModified  = await db.ExecuteAsync("exec [_spChangePassword] @Id, @Password", new { Id = userId, Password = passwordHash });
+
+				if (rowsModified > 0) return new GenericResponse { Success = true };
+
+				return new GenericResponse { Success = false, ErrorMesage = "We are exipirencing problems on the server. Try again later" };
 			}
+		}
+
+		public async Task<UserModel> LoginAsync(LoginRequest loginRequest)
+		{
+			string passwordSql = "exec [_spLoginUser] @Email_UserName";
+
+			UserModel user = new UserModel();
+
+			using (SqlConnection db = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+			{
+				await db.OpenAsync();
+
+				user = (await db.QueryAsync<UserModel>(passwordSql, new { loginRequest.Email_UserName })).FirstOrDefault();
+
+			}
+
+			if (user == null || user.Password == null) return null;
+
+			if (!PasswordHashing.ComparePasswords(user.Password, loginRequest.Password))
+			{
+				return null;
+			}
+
+			return user;
 
 		}
 
-		public async  Task<bool> RegisterAsync(UserRequest userRequest)
+		public async Task<GenericResponse> RegisterAsync(UserRequest userRequest)
 		{
-			string sql = "exec _spRegisterUser @Email @UserName @Password @Confirm_Password";
+			string sql = "exec [_spRegisterUser] @Id, @Email, @UserName, @Password";
+
+			if (userRequest.Confirm_Password != userRequest.Password)
+				return new GenericResponse {
+					Success = false,
+					ErrorMesage = "The Passwords do not match !"
+				};
 
 			using (SqlConnection db = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
 			{
 				await db.OpenAsync();
 
-				int rowsModified = await db.ExecuteAsync(sql, userRequest);
+				// Check if the email is in use
 
-				return rowsModified > 0;
+				string EmailInUse = (await db.QueryAsync<string>("exec [_spCheckInUseEmail] @Email", new { userRequest.Email })).FirstOrDefault();
+
+				if (EmailInUse != null)
+				{
+					return new GenericResponse
+					{
+						Success = false,
+						ErrorMesage = "This Email is already in use !"
+					};
+				}
+
+				// Check if the username is in use
+
+				string UserNameInUse = (await db.QueryAsync<string>("exec [_spCheckInUseUserName] @UserName", new { userRequest.UserName })).FirstOrDefault();
+
+				if (UserNameInUse != null)
+				{
+					return new GenericResponse
+					{
+						Success = false,
+						ErrorMesage = "This Username is already in use !"
+					};
+				}
+
+			}
+
+			string passwordHash = PasswordHashing.HashPassword(userRequest.Password);
+
+			UserModel user = new UserModel
+			{
+				Id = Guid.NewGuid().ToString(),
+				Email = userRequest.Email,
+				Password = passwordHash,
+				UserName = userRequest.UserName
+			};
+
+			using (SqlConnection db = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+			{
+				await db.OpenAsync();
+
+				int rowsModified = await db.ExecuteAsync(sql, user);
+
+				if (rowsModified > 0)
+				{
+					return new GenericResponse
+					{
+						Success = true
+					};
+				}
+
+				return new GenericResponse
+				{
+					Success = false,
+					ErrorMesage = "Problem with registering the user"
+				};
+
 			}
 		}
 	}
