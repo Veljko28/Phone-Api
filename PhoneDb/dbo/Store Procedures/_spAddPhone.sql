4d6022cc2e7e91308ad2bb2d4db885ab14bf45c5
Veljko28 <charlibear28@gmail.com>
2021-07-20 07:52:52 +0200

    Phone Store Procedures

--- PhoneDb/dbo/Store Procedures/_spAddPhone.sql
+++ PhoneDb/dbo/Store Procedures/_spAddPhone.sql
@@ -1,11 +1,12 @@
 ﻿CREATE PROCEDURE [dbo].[_spAddPhone]
 	@Id NVARCHAR(50),
 	@Seller NVARCHAR(50),
+	@Price MONEY,
 	@Name NVARCHAR(100),
 	@Image NVARCHAR(150),
 	@Category NVARCHAR(50),
 	@Brand NVARCHAR(25)
 AS
 begin
-	INSERT INTO [dbo].[Phones] (Id, Seller, [Name], [Image], Category, Brand) VALUES (@Id, @Seller, @Name, @Image, @Category, @Brand);
+	INSERT INTO [dbo].[Phones] (Id, Seller, Price, [Name], [Image], Category, Brand) VALUES (@Id, @Seller, @Price, @Name, @Image, @Category, @Brand);
 end
---
+++ PhoneDb/dbo/Store Procedures/_spPhoneImageUpload.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[_spPhoneImageUpload]
+	@Id NVARCHAR(50),
+	@Image NVARCHAR(256)
+AS
+begin
+  UPDATE [dbo].[Phones] SET [Image] = @Image WHERE Id = @Id;
+end
