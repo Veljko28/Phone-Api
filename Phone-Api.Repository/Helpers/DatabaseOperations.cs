c77009bd2be5527e9c81ddc7f4d50fae2b8c298e
Veljko28 <charlibear28@gmail.com>
2021-07-21 07:41:03 +0200

    Static class with database opertaions

---
+++ Phone-Api.Repository/Helpers/DatabaseOperations.cs
@@ -0,0 +1,58 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using Phone_Api.Models.Responses;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phone_Api.Repository.Helpers
+{
+	public static class DatabaseOperations
+	{
+
+		public static async Task<GenericResponse> GenericExecute<T>(string sql, T payload, IConfiguration _configuration, string ErrorMessage)
+		{
+			using (SqlConnection db = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+			{
+				await db.OpenAsync();
+
+				int rowsModified = await db.ExecuteAsync(sql, payload);
+
+				if (rowsModified != 0) return new GenericResponse { Success = true };
+
+				return new GenericResponse { Success = false, ErrorMessage = ErrorMessage };
+			}
+		}
+
+		public static async Task<IEnumerable<N>> GenericQueryList<T,N>(string sql, T payload, IConfiguration _configuration)
+		{
+			using (SqlConnection db = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+			{
+				await db.OpenAsync();
+
+				IEnumerable<N> list = await db.QueryAsync<N>(sql, payload);
+
+				return list;
+			}
+		}
+
+
+		public static async Task<T> GenericQuerySingle<T>(string sql, T payload, IConfiguration _configuration)
+		{
+			using (SqlConnection db = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+			{
+				await db.OpenAsync();
+
+				T list = (await db.QueryAsync<T>(sql, payload)).FirstOrDefault();
+
+				return list;
+			}
+		}
+
+
+
+	}
+}
