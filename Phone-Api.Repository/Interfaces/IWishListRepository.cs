71268f55db6468b2f15591e9ead1476562fd7e8c
Veljko28 <charlibear28@gmail.com>
2021-07-21 07:40:49 +0200

    Wish List Repository Implementation

---
+++ Phone-Api.Repository/Interfaces/IWishListRepository.cs
@@ -0,0 +1,17 @@
+﻿using Phone_Api.Models;
+using Phone_Api.Models.Requests;
+using Phone_Api.Models.Responses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phone_Api.Repository.Interfaces
+{
+	public interface IWishListRepository
+	{
+		Task<GenericResponse> AddToWishListAsync(WishListRequest model);
+		Task<GenericResponse> RemoveFromWishListAsync(string Id);
+		Task<IEnumerable<string>> GetUserWishesAsync(string userId);
+	}
+}
---
+++ Phone-Api.Repository/WishListRepository.cs
@@ -0,0 +1,53 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using Phone_Api.Models;
+using Phone_Api.Models.Requests;
+using Phone_Api.Models.Responses;
+using Phone_Api.Repository.Helpers;
+using Phone_Api.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phone_Api.Repository
+{
+	public class WishListRepository : IWishListRepository
+	{
+		private readonly IConfiguration _configuration;
+
+		public WishListRepository(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+
+		public async Task<GenericResponse> AddToWishListAsync(WishListRequest model)
+		{
+			WishListModel wish = new WishListModel
+			{
+				Id = Guid.NewGuid().ToString(),
+				UserId = model.UserId,
+				PhoneId = model.PhoneId
+			};
+
+			string sql = "exec [_spAddToWishList] @Id, @UserId, @PhoneId";
+			return await DatabaseOperations.GenericExecute(sql, wish, _configuration, "Failed to insert the item to the wish list");
+		}
+
+		public async Task<IEnumerable<string>> GetUserWishesAsync(string UserId)
+		{
+			string sql = "exec [_spGetUserWishes] @UserId";
+
+			return await DatabaseOperations.GenericQueryList<dynamic,string>(sql, new { UserId }, _configuration);
+		}
+
+		public async Task<GenericResponse> RemoveFromWishListAsync(string Id)
+		{
+			string sql = "exec [_spRemoveWish] @Id";
+
+			return await DatabaseOperations.GenericExecute(sql, new { Id }, _configuration, "Failed to remove the item from the wish list");
+
+		}
+	}
+}
