39cc315b435bc1616a2ab6faf72d9b04dd2b5d28
Veljko28 <charlibear28@gmail.com>
2021-07-22 11:27:22 +0200

    Generic Request Folder Added

---
+++ Phone-Api.Models/Requests/GenericRequests/ChangePasswordRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phone_Api.Models.Requests
+{
+	public class ChangePasswordRequest
+	{
+		public string Old_Password { get; set; }
+		public string Confirm_Old_Password { get; set; }
+		public string New_Password { get; set; }
+	}
+}
---
+++ Phone-Api.Models/Requests/GenericRequests/UploadRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phone_Api.Models.Requests
+{
+
+	public enum uploadTypes
+	{
+		User,
+		Phone
+	}
+
+	public class UploadRequest
+	{
+		public uploadTypes Type { get; set; }
+		public string Id { get; set; }
+	}
+}
