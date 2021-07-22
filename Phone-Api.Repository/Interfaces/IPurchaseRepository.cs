f6752a0a4ede0fb3e0e72929e21b4325065ddd19
Veljko28 <charlibear28@gmail.com>
2021-07-22 11:26:19 +0200

    Purchases Repository and interface

---
+++ Phone-Api.Repository/Interfaces/IPurchaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using Phone_Api.Models;
+using Phone_Api.Models.Responses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phone_Api.Repository.Interfaces
+{
+	public interface IPurchaseRepository
+	{
+		Task<string> AddPurchaseAsync(PurchaseRequest req);
+		Task<IEnumerable<string>> GetPurchasePhonesAsync(string purchaseId);
+		Task<GenericResponse> AddPhoneToPurchaseAsync(string purchaseId, string phoneId);
+
+
+	}
+}
---
+++ Phone-Api.Repository/PurchaseRepository.cs
@@ -0,0 +1,62 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using Phone_Api.Models;
+using Phone_Api.Models.Responses;
+using Phone_Api.Repository.Helpers;
+using Phone_Api.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phone_Api.Repository
+{
+	public class PurchaseRepository : IPurchaseRepository
+	{
+		private readonly IConfiguration _configuration;
+
+		public PurchaseRepository(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+
+		public async Task<GenericResponse> AddPhoneToPurchaseAsync(string PurchaseId, string PhoneId)
+		{
+			string sql = "exec [_spAddPhoneToPurchase] @PurchaseId, @PhoneId";
+
+			return await DatabaseOperations.GenericExecute(sql, new { PurchaseId , PhoneId }, _configuration, "Failed to add phone to purchase list");
+		}
+
+		public async Task<string> AddPurchaseAsync(PurchaseRequest req)
+		{
+			string sql = "exec [_spAddPurchase] @Id, @UserId, @Total, @PurchaseDate";
+
+			PurchaseModel model = new PurchaseModel
+			{
+				Id = Guid.NewGuid().ToString(),
+				UserId = req.UserId,
+				Total = req.Total,
+				PurchaseDate = req.PurchaseDate
+			};
+
+			using (SqlConnection db = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+			{
+				await db.OpenAsync();
+
+				int rowsModified = await db.ExecuteAsync(sql, model);
+
+				if (rowsModified > 0) return model.Id;
+
+				return null;
+			}
+		}
+
+		public async Task<IEnumerable<string>> GetPurchasePhonesAsync(string PurchaseId)
+		{
+			string sql = "exec [_spGetPurchasedPhones] @PurchaseId";
+
+			return await DatabaseOperations.GenericQueryList<dynamic, string>(sql, new { PurchaseId }, _configuration);
+		}
+	}
+}
