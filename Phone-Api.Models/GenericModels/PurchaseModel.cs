2c859f4650eb17329de9168329bc5c63fe365d47
Veljko28 <charlibear28@gmail.com>
2021-07-22 11:26:41 +0200

    Purchase Model and Request

---
+++ Phone-Api.Models/GenericModels/PurchaseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phone_Api.Models
+{
+	public class PurchaseModel
+	{
+		public string Id { get; set; }
+		public string UserId { get; set; }
+		public Decimal Total { get; set; }
+		public DateTime PurchaseDate { get; set; }
+	}
+}
---
+++ Phone-Api.Models/Requests/PurchaseRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phone_Api.Models
+{
+	public class PurchaseRequest
+	{
+		public string UserId { get; set; }
+		public Decimal Total { get; set; }
+		public DateTime PurchaseDate { get; set; }
+	}
+}
