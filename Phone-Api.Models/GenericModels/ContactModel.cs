cf710425dd6b0ef39e78135c6afb1ebf8aac90af
Veljko28 <charlibear28@gmail.com>
2021-07-22 11:27:34 +0200

    Generic Models Folder Added

---
+++ Phone-Api.Models/GenericModels/ContactModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phone_Api.Models
+{
+	public class ContactModel
+	{
+		public string Id { get; set; }
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public string Subject { get; set; }
+		public string Message { get; set; }
+
+	}
+}
