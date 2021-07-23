3028adf8f0838d6a78d028e004ad448fcb3b4892
Veljko28 <charlibear28@gmail.com>
2021-07-23 10:18:18 +0200

    Bid Models with Id

---
+++ Phone-Api.Models/BidModels/BidHistoryModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phone_Api.Models.BidModels
+{
+	public class BidHistoryModel
+	{
+		public string Id { get; set; }
+		public string Bid_Id { get; set; }
+		public string UserName { get; set; }
+		public Decimal Amount { get; set; }
+
+	}
+}
---
+++ Phone-Api.Models/BidModels/BidModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phone_Api.Models
+{
+	public class BidModel
+	{
+		public string Id { get; set; }
+		public string Image { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public Decimal Price { get; set; }
+		public string Seller { get; set; }
+		public string Category { get; set; }
+		public string Brand { get; set; }
+		public DateTime TimeCreated { get; set; }
+		public DateTime TimeEnds { get; set; }
+
+	}
+}
