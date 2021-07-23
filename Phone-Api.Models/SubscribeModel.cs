ba02035ebead1053018f219dda986909cf38b9b5
Veljko28 <charlibear28@gmail.com>
2021-07-23 10:19:10 +0200

    Email Subscribe Model

---
+++ Phone-Api.Models/SubscribeModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phone_Api.Models
+{
+	public class SubscribeModel
+	{
+		public string Id { get; set; }
+		public string Email { get; set; }
+	}
+}
