725be5f9e3152410327fda72df1684dc7e5a7927
Veljko28 <charlibear28@gmail.com>
2021-07-25 13:30:58 +0200

    Removeing image property from phone models

--- Phone-Api.Models/PhoneModel.cs
+++ Phone-Api.Models/PhoneModel.cs
@@ -7,8 +7,8 @@ namespace Phone_Api.Models
 	public class PhoneModel
 	{
 		public string Id { get; set; }
-		public string Image { get; set; }
 		public string Name { get; set; }
+		public string Description { get; set; }
 		public Decimal Price { get; set; }
 		public string Seller { get; set; }
 		public string Category { get; set; }
--- Phone-Api.Models/Requests/PhoneRequest.cs
+++ Phone-Api.Models/Requests/PhoneRequest.cs
@@ -6,8 +6,8 @@ namespace Phone_Api.Models.Requests
 {
 	public class PhoneRequest
 	{
-		public string Image { get; set; }
 		public string Name { get; set; }
+		public string Description { get; set; }
 		public Decimal Price { get; set; }
 		public string Category { get; set; }
 		public string Brand { get; set; }
