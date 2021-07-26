a8576a72211a3622605df36bd69179f4dd54004b
Veljko28 <charlibear28@gmail.com>
2021-07-26 08:49:54 +0200

    all the phone stored procedures

---
+++ PhoneDb/dbo/Store Procedures/Phone/_spAddPhone.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[_spAddPhone]
+	@Id NVARCHAR(50),
+	@Seller NVARCHAR(50),
+	@Price MONEY,
+	@Description NVARCHAR(256),
+	@Name NVARCHAR(100),
+	@Category NVARCHAR(50),
+	@Brand NVARCHAR(25)
+AS
+begin
+	INSERT INTO [dbo].[Phones] (Id, Seller, Price, [Description], [Name], Category, Brand) VALUES (@Id, @Seller, @Price, @Description, @Name, @Category, @Brand);
+end
---
+++ PhoneDb/dbo/Store Procedures/Phone/_spSellerPhonesById.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[_spSellerPhonesById]
+	@Id NVARCHAR(50)
+AS
+begin
+	SELECT * FROM [dbo].[Phones] WHERE Seller = @Id;
+end
