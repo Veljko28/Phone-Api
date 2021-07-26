a4d967cc616fa43567f78db2efcaeabe85503723
Veljko28 <charlibear28@gmail.com>
2021-07-26 08:51:29 +0200

    Wish List Stored Procedures

---
+++ PhoneDb/dbo/Store Procedures/WishList/_spAddToWishList.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[_spAddToWishList]
+	@Id NVARCHAR(50),
+	@UserId NVARCHAR(50),
+	@PhoneId NVARCHAR(50)
+AS
+begin
+	INSERT INTO [dbo].[WishLists] (Id, UserId, PhoneId) VALUES (@Id, @UserId, @PhoneId);
+end
---
+++ PhoneDb/dbo/Store Procedures/WishList/_spGetUserWishes.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[_spGetUserWishes]
+	@UserId NVARCHAR(50)
+AS
+begin
+	SELECT PhoneId FROM [dbo].[WishLists] WHERE UserId = @UserId;
+end
