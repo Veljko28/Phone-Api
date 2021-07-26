5139d45cccb35cc45b7a77254ffc0bc0c4ea9200
Veljko28 <charlibear28@gmail.com>
2021-07-26 08:49:42 +0200

    all the generic stored procedures

---
+++ PhoneDb/dbo/Store Procedures/Generic/_spAddSubscribeEmail.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[_spAddSubscribeEmail]
+	@Id NVARCHAR(50),
+	@Email NVARCHAR(256)
+AS
+begin
+	INSERT INTO [dbo].[SubscribeEmails] (Id, Email) VALUES (@Id, @Email);
+end
---
+++ PhoneDb/dbo/Store Procedures/Generic/_spContactSupport.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[_spContactSupport]
+	@Id NVARCHAR(50),
+    @Name NVARCHAR(100),
+    @Email NVARCHAR(150),
+    @Subject NVARCHAR(100), 
+    @Message NVARCHAR(MAX)
+AS
+begin
+	INSERT INTO [dbo].[ContactSupport] (Id, [Name], [Email], [Subject], [Message]) VALUES (@Id, @Name, @Email, @Subject, @Message);
+end
