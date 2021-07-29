d70fde21483d52e0710f286cff6f2dfe5641a299
Veljko28 <charlibear28@gmail.com>
2021-07-29 13:05:03 +0200

    Token Models for jwt

---
+++ Phone-Api.Models/Responses/TokenResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Phone_Api.Models.Responses
+{
+	public class TokenResponse
+	{
+		[Required]
+		public string Token { get; set; }
+
+		[Required]
+		public string RefreshToken { get; set; }
+
+		[Required]
+		public string Expires { get; set; }
+	}
+}
