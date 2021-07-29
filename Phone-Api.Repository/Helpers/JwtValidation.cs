83757ea261730f8318df59bd0f7ce6d0740e57f1
Veljko28 <charlibear28@gmail.com>
2021-07-29 13:05:26 +0200

    Getting the jwt setting and validating the jwt token

---
+++ Phone-Api.Repository/Helpers/JwtValidation.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Phone_Api.Helpers;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Phone_Api.Repository.Helpers
+{
+	public static class JwtValidation
+	{
+		private static bool isJwtWithValidSecurityAlgorithm(SecurityToken validatedToken)
+		{
+			return (validatedToken is JwtSecurityToken jwtSecurityToken) &&
+				jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase);
+		}
+
+		public static ClaimsPrincipal getPrincipalFromToken(string token, IConfiguration configuration)
+		{
+			var tokenHandler = new JwtSecurityTokenHandler();
+
+			var jwtSettings = new JwtSettings();
+			configuration.Bind(nameof(JwtSettings), jwtSettings);
+
+			try
+			{
+
+				var tokenValidationParameters = new TokenValidationParameters
+				{
+					ValidateIssuerSigningKey = true,
+					IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtSettings.Secret)),
+					ValidateIssuer = false,
+					ValidateAudience = false,
+					RequireExpirationTime = false,
+					ValidateLifetime = true
+				};
+
+				var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out var validatedToken);
+				if (!isJwtWithValidSecurityAlgorithm(validatedToken))
+				{
+					return null;
+				}
+
+				return principal;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+				return null;
+			}
+
+		}
+
+	}
+}
