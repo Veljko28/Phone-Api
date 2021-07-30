666b19e21b62dba06460d846c8e7fb950acee407
Veljko28 <charlibear28@gmail.com>
2021-07-30 12:23:36 +0200

    Refresh Token update, create, and find repository methods

---
+++ Phone-Api.Repository/RefreshTokenRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+using Phone_Api.Models;
+using Phone_Api.Models.Responses;
+using Phone_Api.Repository.Helpers;
+using Phone_Api.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phone_Api.Repository
+{
+	public class RefreshTokenRepository : IRefreshTokenRepository
+	{
+		private readonly IConfiguration _configuration;
+
+		public RefreshTokenRepository(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+
+		public async Task<bool> AddTokenAsync(RefreshToken refreshToken)
+		{
+			string sql = "exec [AddRefreshToken] @Token, @JwtId, @CreatedDate, @Expires, @Used, @Invalidated, @UserId";
+
+			return (await DatabaseOperations.GenericExecute(sql, refreshToken, _configuration, "Failed to add the refresh token")).Success;
+
+		}
+
+		public async Task<RefreshToken> FindTokenAsync(string Token)
+		{
+			string sql = "exec [FindRefreshToken] @Token";
+
+			return await DatabaseOperations.GenericQuerySingle<dynamic, RefreshToken>(sql, new { Token }, _configuration);
+		}
+
+		public async Task<bool> UpdateTokenAsync(RefreshToken refreshToken)
+		{
+			string sql = "exec [UpdateRefeshToken] @Token, @JwtId, @CreatedDate, @Expires, @Used, @Invalidated, @UserId";
+
+			return (await DatabaseOperations.GenericExecute(sql, refreshToken, _configuration, "Failed to update the refresh token")).Success;
+		
+		}
+	}
+}
