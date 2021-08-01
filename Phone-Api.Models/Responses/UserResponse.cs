ac68a0d99ee254e353ad462f6d5025c7b8be7e17
Veljko28 <charlibear28@gmail.com>
2021-08-01 13:07:10 +0200

    User Response model without password hash

---
+++ Phone-Api.Models/Responses/UserResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phone_Api.Models.Responses
+{
+	public class UserResponse
+	{
+		public string Id { get; set; }
+		public string Image { get; set; }
+		public string Email { get; set; }
+		public string UserName { get; set; }
+		public string Description { get; set; }
+		public int Phones_sold { get; set; }
+		public string Contanct { get; set; }
+	}
+}
