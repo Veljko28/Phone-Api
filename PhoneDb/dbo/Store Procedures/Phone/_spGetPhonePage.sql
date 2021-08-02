b74b61a39b9437c5f257079684826108b410957f
Veljko28 <charlibear28@gmail.com>
2021-08-02 11:07:53 +0200

    Stored Procedure for getting a phone page

---
+++ PhoneDb/dbo/Store Procedures/Phone/_spGetPhonePage.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[_spGetPhonePage]
+	@Page int
+AS
+begin
+	 SELECT * FROM (
+            SELECT ROW_NUMBER() OVER(ORDER BY Id) AS RoNum
+                  , *
+            FROM [dbo].[Phones]
+    ) AS tbl 
+    WHERE (@Page-1)*15 < RoNum
+    ORDER BY tbl.Id
+end
