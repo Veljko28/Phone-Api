ebfd26bd0e981a22c2b9b578543abaf9b9680baf
Veljko28 <charlibear28@gmail.com>
2021-08-06 13:19:56 +0200

    Edit Phone and Remove Phone Image Stored Procedures

---
+++ PhoneDb/dbo/Store Procedures/Phone/_spEditPhone.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[_spEditPhone]
+	@Id NVARCHAR(50),
+	@Image NVARCHAR(256),
+	@Name NVARCHAR(100),
+	@Description NVARCHAR(256),
+	@Price MONEY,
+	@Seller NVARCHAR(50),
+	@Category NVARCHAR(50),
+	@DateCreated DATETIME2(7),
+	@Brand NVARCHAR(25),
+	@Status NVARCHAR(50)
+AS
+begin
+	UPDATE [dbo].[Phones] SET [Image] = @Image, [Name] = @Name, [Description] = @Description, Price = @Price, Seller =  @Seller, Category = @Category,
+	DateCreated = @DateCreated, Brand = @Brand, [Status] = @Status WHERE Id = @Id
+end
---
+++ PhoneDb/dbo/Store Procedures/Phone/_spRemovePhoneImage.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[_spRemovePhoneImage]
+	@ImagePath NVARCHAR(256),
+	@PhoneId NVARCHAR(50)
+AS
+begin
+	DELETE FROM [dbo].[PhoneImages] WHERE ImagePath = @ImagePath AND PhoneId = @PhoneId;
+end
