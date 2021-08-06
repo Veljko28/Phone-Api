c348d0fdbfd9f8ba98c616a976a141f99e5459d7
Veljko28 <charlibear28@gmail.com>
2021-08-06 13:20:05 +0200

    Edit Phone Model

---
+++ Phone-Api.Models/EditModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phone_Api.Models
+{
+	public class EditModel
+	{
+		public PhoneModel Model { get; set; }
+		public IEnumerable<string> Images { get; set; }
+	}
+}
