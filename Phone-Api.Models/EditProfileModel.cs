2a7f18feb79f846d86fbd9538d4e576ed317803d
Veljko28 <charlibear28@gmail.com>
2021-08-11 12:25:27 +0200

    Edit user profile model and stored procedure

---
+++ Phone-Api.Models/EditProfileModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phone_Api.Models
+{
+	public class EditProfileModel
+	{
+		public string UserName { get; set; }
+		public string Email { get; set; }
+		public string Description { get; set; }
+	}
+}
---
+++ PhoneDb/dbo/Store Procedures/User/_spEditUserProfile.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[_spEditUserProfile]
+	@Id NVARCHAR(50),
+	@UserName NVARCHAR(150),
+	@Email NVARCHAR(150),
+	@Description NVARCHAR(256)
+AS
+begin
+	UPDATE [dbo].[Users] SET UserName = @UserName, Email = @Email, [Description] = @Description WHERE Id = @Id
+end
