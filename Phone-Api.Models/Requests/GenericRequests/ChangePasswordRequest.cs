420fa45a7d31be0c2717a2adc9e459f8f83be5bb
Veljko28 <charlibear28@gmail.com>
2021-08-13 13:00:28 +0200

    Change Password request model and removing image from user response

--- Phone-Api.Models/Requests/GenericRequests/ChangePasswordRequest.cs
+++ Phone-Api.Models/Requests/GenericRequests/ChangePasswordRequest.cs
@@ -6,8 +6,8 @@ namespace Phone_Api.Models.Requests
 {
 	public class ChangePasswordRequest
 	{
-		public string Old_Password { get; set; }
-		public string Confirm_Old_Password { get; set; }
+		public string Current_Password { get; set; }
+		public string Confirm_Current_Password { get; set; }
 		public string New_Password { get; set; }
 	}
 }
--- Phone-Api.Models/Responses/UserResponse.cs
+++ Phone-Api.Models/Responses/UserResponse.cs
@@ -7,7 +7,6 @@ namespace Phone_Api.Models.Responses
 	public class UserResponse
 	{
 		public string Id { get; set; }
-		public string Image { get; set; }
 		public string Email { get; set; }
 		public string UserName { get; set; }
 		public string Description { get; set; }
