14d3e1260fffdb9b1f624adcc2798be01920d8f9
Veljko28 <charlibear28@gmail.com>
2021-08-13 12:59:31 +0200

    Review Stored Procedures for adding and getting phone reviews

---
+++ PhoneDb/dbo/Store Procedures/Generic/_spAddReview.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[_spAddReview]
+	@Id NVARCHAR(50),
+	@Message NVARCHAR(256),
+	@Rating int,
+	@UserId NVARCHAR(50),
+	@PhoneId NVARCHAR(50),
+	@DateCreated DATETIME2(7)
+AS
+begin
+	INSERT INTO [dbo].[PhoneReviews] (Id, Rating, [Message], UserId, PhoneId, DateCreated) 
+	VALUES (@Id, @Rating, @Message, @UserId, @PhoneId, @DateCreated);
+end
---
+++ PhoneDb/dbo/Store Procedures/Generic/_spGetPhoneReviewsById.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[_spGetPhoneReviewsById]
+	@Id NVARCHAR(50)
+AS
+begin
+	SELECT * FROM [dbo].[PhoneReviews] WHERE PhoneId = @Id;
+end
