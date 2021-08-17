08127a61e878ed2914327fc0a1573a593c4455ea
Veljko28 <charlibear28@gmail.com>
2021-08-17 13:24:33 +0200

    Remove from wish list request model and stored procedure for deleting it

--- PhoneDb/dbo/Store Procedures/WishList/_spRemoveWish.sql
+++ PhoneDb/dbo/Store Procedures/WishList/_spRemoveWish.sql
@@ -1,6 +1,7 @@
 ﻿CREATE PROCEDURE [dbo].[_spRemoveWish]
-	@Id NVARCHAR(50)
+	@UserId NVARCHAR(50),
+	@PhoneId NVARCHAR(50)
 AS
 begin
-	DELETE FROM [dbo].[WishLists] WHERE Id = @Id;
+	DELETE FROM [dbo].[WishLists] WHERE UserId = @UserId AND PhoneId = @PhoneId;
 end
