d51da2adb48822345893a20e50253f16b2f9731c
Veljko28 <charlibear28@gmail.com>
2021-08-20 09:53:09 +0200

    Notifications in Repository

---
+++ Phone-Api.Repository/Interfaces/INotificationRepository.cs
@@ -0,0 +1,19 @@
+﻿using Phone_Api.Models;
+using Phone_Api.Models.Requests;
+using Phone_Api.Models.Responses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phone_Api.Repository.Interfaces
+{
+	public interface INotificationRepository
+	{
+		Task<IEnumerable<NotificationModel>> GetUserNotificationsAsync(string userId);
+
+		Task<GenericResponse> AddNotificationAsnyc(NotificationModelRequest model);
+		Task<GenericResponse> RemoveNotificationAsnyc(string Id);
+
+	}
+}
---
+++ Phone-Api.Repository/NotificationRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Configuration;
+using Phone_Api.Models;
+using Phone_Api.Models.Requests;
+using Phone_Api.Models.Responses;
+using Phone_Api.Repository.Helpers;
+using Phone_Api.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phone_Api.Repository
+{
+	public class NotificationRepository : INotificationRepository
+	{
+		private readonly IConfiguration _configuration;
+
+		public NotificationRepository(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+
+		public async Task<GenericResponse> AddNotificationAsnyc(NotificationModelRequest model)
+		{
+			NotificationModel responseModel = new NotificationModel
+			{
+				Id = Guid.NewGuid().ToString(),
+				Type = model.Type,
+				Name = model.Name,
+				UserId = model.UserId,
+				Message = model.Message
+			};
+
+			string sql = "exec [_spAddNotifications] @Id, @Type, @Name, @UserId, @Message";
+
+			return await DatabaseOperations.GenericExecute(sql, responseModel, _configuration, "Failed to add notification");
+		}
+
+		public async Task<IEnumerable<NotificationModel>> GetUserNotificationsAsync(string userId)
+		{
+			string sql = "exec [_spGetUserNotifications] @Id";
+
+			return await DatabaseOperations.GenericQueryList<dynamic, NotificationModel>(sql, new { Id = userId }, _configuration);
+		}
+
+		public async Task<GenericResponse> RemoveNotificationAsnyc(string Id)
+		{
+
+			string sql = "exec [_spRemoveNotifications] @Id";
+
+			return await DatabaseOperations.GenericExecute(sql, new { Id }, _configuration, "Failed to remove notification");
+		}
+	}
+}
