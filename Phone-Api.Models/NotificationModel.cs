05d9511fbe174c3dee3c3657b3e249c2650b1f17
Veljko28 <charlibear28@gmail.com>
2021-08-20 09:53:26 +0200

    Models for notifications and requests

---
+++ Phone-Api.Models/NotificationModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phone_Api.Models
+{
+	public class NotificationModel
+	{
+		public string Id { get; set; }
+		public string Type { get; set; }
+		public string Name { get; set; }
+		public string UserId { get; set; }
+		public string Message { get; set; }
+
+	}
+}
