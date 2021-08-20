73bda687014d25a04a4e9282baa2098474c45958
Veljko28 <charlibear28@gmail.com>
2021-08-20 15:36:35 +0200

    Adding the singleton and controller for mails

---
+++ Phone-Api/Controllers/MailController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Phone_Api.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phone_Api.Controllers
+{
+	public class MailController : Controller
+	{
+		private readonly IMailService _mail;
+
+		public MailController(IMailService mail)
+		{
+			_mail = mail;
+		}
+
+		[HttpPost("/email")]
+		public async Task<IActionResult> SendEmail()
+		{
+			await _mail.SendCofirmEmailAsync("charlibear284@gmail.com");
+
+			return Ok();
+		}
+	}
+}
--- Phone-Api/Installers/RepositoryInstaller.cs
+++ Phone-Api/Installers/RepositoryInstaller.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Phone_Api.Repository;
+using Phone_Api.Repository.Helpers;
 using Phone_Api.Repository.Interfaces;
+using Phone_Api.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,9 +15,16 @@ namespace Phone_Api.Installers
 	{
 		public void InstallServices(IConfiguration configuration, IServiceCollection services)
 		{
+			var SmtpSettings = new SmtpSettings();
+			configuration.Bind(nameof(SmtpSettings), SmtpSettings);
+			services.AddSingleton(SmtpSettings);
+
+			//services.Configure<SmtpSettings>(configuration.GetSection("SmtpSettings"));
+
 			services.AddSingleton<IPhoneRepository, PhoneRepository>();
 			services.AddSingleton<IBidRepository, BidRepository>();
 			services.AddScoped<IUserRepository, UserRepository>();
+			services.AddSingleton<IMailService, MailService>();
 
 
 			services.AddSingleton<IWishListRepository, WishListRepository>();
