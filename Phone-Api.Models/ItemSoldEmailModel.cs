16d2bec8eca939b3f877b4f971a4e478a4b92c63
Veljko28 <charlibear28@gmail.com>
2021-08-21 10:25:30 +0200

    Item sold Model for getting email info from user on controller

---
+++ Phone-Api.Models/ItemSoldEmailModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phone_Api.Models
+{
+	public class ItemSoldEmailModel
+	{
+		public string Email { get; set; }
+		public string BuyerId { get; set; }
+		public string Type { get; set; }
+		public string ItemName { get; set; }
+	}
+}
