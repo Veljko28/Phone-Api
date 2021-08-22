87c15804567e80bcd49c96020c21717d6bce2b1c
Veljko28 <charlibear28@gmail.com>
2021-08-22 09:13:52 +0200

    New Confirm Email model with user email and confirm email id

---
+++ Phone-Api.Models/EmailModels/ConfirmEmailModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phone_Api.Models.EmailModels
+{
+	public class ConfirmEmailModel
+	{
+		public string Email { get; set; }
+		public string ConfirmEmailId { get; set; }
+	}
+}
