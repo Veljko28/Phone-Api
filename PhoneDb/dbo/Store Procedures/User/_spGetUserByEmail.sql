7eafc0318ec51dcb0f3e43b65afeb6423e721b94
Veljko28 <charlibear28@gmail.com>
2021-08-23 13:23:07 +0200

    New user stored procedures

---
+++ PhoneDb/dbo/Store Procedures/User/_spGetUserByEmail.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[_spGetUserByEmail]
+	@Email NVARCHAR(150)
+AS
+begin 
+	SELECT * FROM [dbo].[Users] WHERE Email = @Email;
+end
---
+++ PhoneDb/dbo/Store Procedures/User/_spGetUserEmailById.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[_spGetUserEmailById]
+	@Id NVARCHAR(50)
+AS
+begin
+	SELECT Email from [dbo].[Users] WHERE Id = @Id;
+end	
