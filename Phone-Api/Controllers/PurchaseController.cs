93c1855383ac4eafc1e401376c910dd2e2fe1c2b
Veljko28 <charlibear28@gmail.com>
2021-08-25 13:13:45 +0200

    Updateing the purchase controller for the new routes

--- Phone-Api/Controllers/PurchaseController.cs
+++ Phone-Api/Controllers/PurchaseController.cs
@@ -15,50 +15,46 @@ namespace Phone_Api.Controllers
 	public class PurchaseController : Controller
 	{
 		private readonly IPurchaseRepository _purchases;
+		private readonly IPhoneRepository _phones;
 
-		public PurchaseController(IPurchaseRepository purchases)
+		public PurchaseController(IPurchaseRepository purchases, IPhoneRepository phones)
 		{
 			_purchases = purchases;
+			_phones = phones;
 		}
 
 		[HttpPost(ApiRoutes.PurchaseRoutes.AddPurchase)]
-		public async Task<IActionResult> AddPurchase([FromBody] PurchaseRequest req)
+		public async Task<IActionResult> AddPurchase([FromBody] IEnumerable<PurchaseRequest> req)
 		{
 			var result = await _purchases.AddPurchaseAsync(req);
 
-			if (result == null) return BadRequest("Failed to add purchase to database");
+			if (!result.Success) return BadRequest(result.ErrorMessage);
 
-			return Ok(result);
+			return Ok();
 		}
 
-		[HttpGet(ApiRoutes.PurchaseRoutes.GetPurchasePhones)]
-		public async Task<IActionResult> GetPurchasePhones([FromRoute] string purchaseId)
+		[HttpGet(ApiRoutes.PurchaseRoutes.GetPurchasedPhones)]
+		public async Task<IActionResult> GetPurchasedPhones([FromRoute] string userId, [FromRoute] int page)
 		{
-			var results = await _purchases.GetPurchasePhonesAsync(purchaseId);
-			
-			if (!results.Any())
+			IEnumerable<string> phoneIds = await _purchases.GetPurchasedPhonesPageAsync(userId, page);
+
+			List<PhoneModel> phones = new List<PhoneModel>();
+
+			foreach (string phoneId in phoneIds)
 			{
-				return BadRequest("Cannot find any phones for this purchase");
+				PhoneModel phone = await _phones.GetPhoneByIdAsync(phoneId);
+
+				if (phone != null) phones.Add(phone);
+			}
+			if (page == 1)
+			{
+				int numOfPages = await _purchases.GetNumOfPagesAsync(userId);
+
+				return Ok(new { phones, numOfPages });
+
 			}
 
-			return Ok(results);
+			return Ok(phones);
 		}
-
-
-		[HttpPost(ApiRoutes.PurchaseRoutes.AddPhoneToPurchase)]
-		public async Task<IActionResult> AddPhoneToPurchase([FromRoute] string purchaseId, [FromRoute] string phoneId)
-		{
-			var result = await _purchases.AddPhoneToPurchaseAsync(purchaseId, phoneId);
-
-			if (!result.Success)
-			{
-				return BadRequest(result.ErrorMessage);
-			}
-
-			return Ok("Successfully added purchase");
-		}
-
-
-
 	}
 }
