2b60843f5030c5389ec14e3ad18ca88ba6e29615
Veljko28 <charlibear28@gmail.com>
2021-08-28 13:33:06 +0200

    get user wishlist and num of user phones stored procedures

---
+++ PhoneDb/dbo/Store Procedures/Phone/_spGetNumberOfUserPhones.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[_spGetNumberOfUserPhones]
+	@UserId NVARCHAR(50)
+AS
+begin
+	SELECT COUNT(*) FROM [dbo].[Phones] WHERE Seller = @UserId AND [Status] = 0;
+end
---
+++ PhoneDb/dbo/Store Procedures/WishList/_spFindUserWishList.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[_spFindUserWishList]
+	@UserId NVARCHAR(50),
+	@PhoneId NVARCHAR(50),
+	@Type NVARCHAR(5)
+AS
+begin
+	SELECT * FROM [dbo].[WishLists] WHERE UserId = @UserId AND PhoneId = @PhoneId AND [Type] = @Type;
+end
