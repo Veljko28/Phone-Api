2509213070d9ac3ec305ce22bee09744d09a6734
Veljko28 <charlibear28@gmail.com>
2021-08-31 16:11:40 +0200

    Review Methods in repository

---
+++ Phone-Api.Repository/Interfaces/IReviewRepository.cs
@@ -0,0 +1,18 @@
+﻿using Phone_Api.Models;
+using Phone_Api.Models.Responses;
+using Phone_Api.Models.ReviewModels.Requests;
+using Phone_Api.Models.ReviewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phone_Api.Repository.Interfaces
+{
+	public interface IReviewRepository
+	{
+		Task<GenericResponse> AddReviewAsync(ReviewModelRequest req);
+		Task<IEnumerable<ReviewModel>> GetUserReviewsAsync(string userId);
+		Task<bool> CheckReviewedAsync(ReviewedCheckRequest request);
+	}
+}
---
+++ Phone-Api.Repository/ReviewRepository.cs
@@ -0,0 +1,56 @@
+﻿using Phone_Api.Models.Responses;
+using Phone_Api.Models.ReviewModels.Requests;
+using Phone_Api.Models.ReviewModels;
+using Phone_Api.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Phone_Api.Repository.Helpers;
+using Microsoft.Extensions.Configuration;
+
+namespace Phone_Api.Repository
+{
+	public class ReviewRepository : IReviewRepository
+	{
+		private readonly IConfiguration _configuration;
+		public ReviewRepository(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+		public async Task<GenericResponse> AddReviewAsync(ReviewModelRequest req)
+		{
+			ReviewModel model = new ReviewModel
+			{
+				Id = Guid.NewGuid().ToString(),
+				Rating = req.Rating,
+				BuyerId = req.BuyerId,
+				SellerId = req.SellerId,
+				PhoneId = req.PhoneId,
+				DateCreated = req.DateCreated,
+				Message = req.Message
+			};
+
+			string sql = "exec [_spAddReview] @Id, @Rating, @BuyerId, @SellerId, @PhoneId, @DateCreated, @Message";
+
+			return await DatabaseOperations.GenericExecute(sql, model, _configuration, "Failed to add the review");
+		}
+
+		public async Task<bool> CheckReviewedAsync(ReviewedCheckRequest request)
+		{
+			string sql = "exec [_spFindUserReview] @BuyerId, @PhoneId";
+
+			var model = await DatabaseOperations.GenericQuerySingle<dynamic, ReviewModel>(sql, request, _configuration);
+
+			return model != null;
+		}
+
+		public async Task<IEnumerable<ReviewModel>> GetUserReviewsAsync(string userId)
+		{
+			string sql = "exec [_spGetUserReviews] @SellerId";
+
+			return await DatabaseOperations.GenericQueryList<dynamic, ReviewModel>(sql, new { SellerId = userId }, _configuration);
+		}
+
+	}
+}
