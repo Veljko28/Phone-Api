5f74ff5c05014d8dd5ad1623f182cfc53bfe8c9f
Veljko28 <charlibear28@gmail.com>
2021-09-04 16:38:08 +0200

    Moving the bid models to their own folder

---
+++ Phone-Api.Models/BidModels/BidRequests/BidPriceUpdateRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phone_Api.Models.Requests.BidRequests
+{
+	public class BidPriceUpdateRequest
+	{
+		public string Id { get; set; }
+		public Decimal Price { get; set; }
+	}
+}
--- Phone-Api.Models/BidModels/BidRequests/BidRequest.cs
+++ Phone-Api.Models/BidModels/BidRequests/BidRequest.cs
@@ -2,20 +2,18 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Phone_Api.Models
+namespace Phone_Api.Models.Requests.BidRequests
 {
-	public class BidModel
+	public class BidRequest
 	{
-		public string Id { get; set; }
 		public string Name { get; set; }
-		public string Image { get; set; }
 		public string Description { get; set; }
+		public string Image { get; set; }
 		public Decimal Price { get; set; }
-		public string Seller { get; set; }
 		public string Category { get; set; }
 		public string Brand { get; set; }
 		public DateTime TimeCreated { get; set; } = DateTime.UtcNow;
 		public DateTime Date_Ends { get; set; } = DateTime.UtcNow;
-		public BidStatus Status { get; set; }
+
 	}
 }
