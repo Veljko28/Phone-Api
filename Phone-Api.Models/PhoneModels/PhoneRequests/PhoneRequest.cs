3f9328c0085392fbfaabd548e15bf622804f14f5
Veljko28 <charlibear28@gmail.com>
2021-09-04 16:38:20 +0200

    Phone models in their own folder

---
+++ Phone-Api.Models/PhoneModels/PhoneRequests/PhoneRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phone_Api.Models.Requests
+{
+	public class PhoneRequest
+	{
+		public string Name { get; set; }
+		public string Image { get; set; }
+		public string Description { get; set; }
+		public Decimal Price { get; set; }
+		public string Category { get; set; }
+		public string Brand { get; set; }
+	}
+}
