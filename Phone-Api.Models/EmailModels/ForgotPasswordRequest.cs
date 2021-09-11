d8e03a7db09ff92cbcf6b8a22e18d34b14a4da28
Veljko28 <charlibear28@gmail.com>
2021-09-11 13:14:06 +0200

    Forogt password request model

---
+++ Phone-Api.Models/EmailModels/ForgotPasswordRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phone_Api.Models.EmailModels
+{
+	public class ForgotPasswordRequest
+	{
+		public string Email { get; set; }
+	}
+}
